c9670fc7ea764bdcf6d285d27c0c368794fc99ad
DESKTOP-B89H4VQ\Alex <alexandr.biliaiev@gmail.com>
2017-04-08 01:22:45 +0200

    First commit

---
+++ Bwg/obj/Release/netcoreapp1.1/PubTmp/Out/wwwroot/js/app.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('bwg', ['bwg.controllers', 'ui.router'])
+
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+$urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/home.html'
+           
+        }).state('about', {
+            url: '/about',
+            templateUrl: 'templates/about.html'
+            
+        })
+
+});
+
+
+
+
+
+angular.module('bwg.filters', []);
+angular.module('bwg.services', []);
+angular.module('bwg.controllers', []);
+
+
+
---
+++ Bwg/obj/Release/netcoreapp1.1/PubTmp/Out/wwwroot/js/controllers/homeController.js
@@ -0,0 +1,16 @@
+﻿angular.module('bwg.controllers').controller('HomeController',
+    function ($scope, $location, $anchorScroll) {
+
+        $scope.submitted = true;
+
+        $scope.goTo = function (tag) {
+            $location.hash(tag);
+            $anchorScroll.yOffset = 60;
+            $anchorScroll();
+        };
+
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+
+    })
